29201b09ee36e6ba3460add68e77f8d2735d51c5
Daniel Malik <daniel.malik@mhsp.solutions>
2025-02-18 23:35:38 +0100

    feat: only add Inconsolata

--- pretix_fontpackfree/apps.py
+++ pretix_fontpackfree/apps.py
@@ -5,13 +5,13 @@ from . import __version__
 
 class PluginApp(AppConfig):
     name = "pretix_fontpackfree"
-    verbose_name = "Fontpack: Free fonts"
+    verbose_name = "Fontpack: Free fonts (+ Inconsolata)"
 
     class PretixPluginMeta:
-        name = "Fontpack: Free fonts"
-        author = "Raphael Michel"
-        description = "Pack of free fonts for pretix"
-        visible = False
+        name = "Fontpack: Free fonts (+ Inconsolata)"
+        author = "Raphael Michel, Daniel Malik"
+        description = "Pack of free fonts for pretix (+ Inconsolata)"
+        visible = True
         version = __version__
         compatibility = "pretix>=4.16.0"
 
